4916cf45bf11464c3c896a92afbe9ee4a15824cc
Aliaksandr Shamko <you@example.com>
2025-05-31 13:29:23 +0200

    create new block skills

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Skill } from "./skill/Skill";
+
+export const Skills = () => {
+  return (
+    <StyledSkills>
+      <SectionTitle>My Skills</SectionTitle>
+      <FlexWrapper wrap="wrap">
+        <Skill iconId={"code"} title={"html5"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+        <Skill iconId={"css"} title={"css3"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+        <Skill iconId={"react"} title={"React"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+        <Skill iconId={"typescript"} title={"typescript"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+        <Skill iconId={"styled-components"} title={"styled-components"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+        <Skill iconId={"figma"} title={"WEB DESIGN  "} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
+      </FlexWrapper>
+    </StyledSkills>
+  );
+};
+
+const StyledSkills = styled.section`
+  min-height: 100vh;
+`;
